5941326f6d06258836662f60d6662d72327f9c5e
MerdiCode <merdigamingtv552@gmail.com>
2024-12-04 00:04:08 +0000

    ss

--- srcs/Budgewise-frontend/src/Charts/ProfitChart.jsx
+++ srcs/Budgewise-frontend/src/Charts/ProfitChart.jsx
@@ -80,121 +80,127 @@ class ProfitChart extends Component {
   
   
     changeOptions = async (val) => {
-   
       let days = [0, 0, 0, 0, 0, 0, 0];
       let weeks = [0, 0, 0, 0, 0];
       let month = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
       let year = [0, 0, 0, 0, 0, 0, 0];
+    
       let daily = await axios.get('https://backendbudgewise.onrender.com/users/getDailyProfit/', { withCredentials: true });
       let weekly = await axios.get('https://backendbudgewise.onrender.com/users/getWeeklyProfit/', { withCredentials: true });
       let monthly = await axios.get('https://backendbudgewise.onrender.com/users/getMonthlyProfit/', { withCredentials: true });
       let yearly = await axios.get('https://backendbudgewise.onrender.com/users/getYerlyProfit/', { withCredentials: true });
-  
+    
       let dailyArr = daily.data;
       let weeklyArr = weekly.data;
       let monthlyArr = monthly.data
       let YearlyArr = yearly.data
-      
+    
       dailyArr.forEach((value) => {
         days[(Number(value.day) + 3) % 7] = value.Data;
       });
-  
-  
+    
       weeklyArr.forEach((value) => {
         weeks[Math.floor(Number(value.day) / 7)] = value.Data;
       });
-  
+    
       monthlyArr.forEach((value) => {
         month[value.month - 1] = value.Data;
       });
       YearlyArr.forEach((value) => {
         year[0] = value.Data;
       });
-  
-  
+    
       const dailySeries = [
         {
           name: "Daily",
           data: days,
         }
       ];
-  
+    
       const weeklySeries = [
         {
           name: "Weekly",
           data: weeks,
         }
       ];
+    
       const MonthlySeries = [
         {
           name: "Monthly",
           data: month,
         }
       ];
+    
       const YearlySeries = [
         {
-          name: "Monthly",
+          name: "Yearly",
           data: year,
         }
       ];
+    
+      let newCategories = [];
       switch (val.target.value) {
         case 'Daily':
-          ApexCharts.exec('basic-area', 'updateOptions', {
-            xaxis: {
-              categories: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
-            },
-          });
+          newCategories = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
           this.setState({
-            series: dailySeries,
+            options: {
+              ...this.state.options,
+              xaxis: { categories: newCategories }
+            },
+            series: dailySeries
           });
           break;
-       
-          case 'Weekly':
-          ApexCharts.exec('basic-area', 'updateOptions', {
-            xaxis: {
-              categories: ['Week 1', 'Week 2', 'Week 3', 'Week 4', 'Week 5'],
-            },
-          });
+    
+        case 'Weekly':
+          newCategories = ['Week 1', 'Week 2', 'Week 3', 'Week 4', 'Week 5'];
           this.setState({
-            series: weeklySeries,
+            options: {
+              ...this.state.options,
+              xaxis: { categories: newCategories }
+            },
+            series: weeklySeries
           });
           break;
-        
-          case 'Monthly':
-          ApexCharts.exec('basic-area', 'updateOptions', {
-            xaxis: {
-              categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-            },
-          });
+    
+        case 'Monthly':
+          newCategories = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
           this.setState({
-  
-  
-            series: MonthlySeries,
+            options: {
+              ...this.state.options,
+              xaxis: { categories: newCategories }
+            },
+            series: MonthlySeries
           });
           break;
-      
-          case 'Yearly':
-          ApexCharts.exec('basic-area', 'updateOptions', {
-            xaxis: {
-              categories: ['2024', '2025', '2026', '2027', '2028', '2029', '2030'],
-            },
-          });
+    
+        case 'Yearly':
+          newCategories = ['2024', '2025', '2026', '2027', '2028', '2029', '2030'];
           this.setState({
-            series: YearlySeries,
+            options: {
+              ...this.state.options,
+              xaxis: { categories: newCategories }
+            },
+            series: YearlySeries
           });
           break;
-          default:
-            ApexCharts.exec('basic-area', 'updateOptions', {
-              xaxis: {
-                categories: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
-              },
-            });
-            this.setState({
-              series: dailySeries,
-            });
-            break;
+    
+        default:
+          newCategories = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+          this.setState({
+            options: {
+              ...this.state.options,
+              xaxis: { categories: newCategories }
+            },
+            series: dailySeries
+          });
+          break;
       }
-    }
+    
+      ApexCharts.exec("basic-area", "updateOptions", {
+        xaxis: { categories: newCategories }
+      });
+    };
+    
 
     render() {
       return (
